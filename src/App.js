4119e4cce379822cfa2b310416646b6422391a3e
hyeswe10 <jmoon981107@gmail.com>
2025-06-24 13:21:22 +0900

    netflix 2차

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import MainLayout from './Component/MainLayout';
 import Main from './Component/Main';
+import Login from './Component/Login';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<MainLayout/>}>
           <Route index element={<Main/>}/>
+          <Route path='login' element={<Login/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,9 +1,21 @@
-import "../style/Main.scss";
+import { Link } from "react-router-dom";
+import Membership from "./Membership";
+
 const Main = () => {
     return (
         <main id="main">
+            <div className="lang">
+                <select>
+                    <option selected>한국어</option>
+                    <option>English</option>
+                </select>
+                <Link to="login">
+                <button>로그인</button>
+                </Link>
+            </div>
             <h1>영화, 시리즈 등을 무제한으로</h1>
             <p>7,000원으로 시작하세요. 멤버십은 언제든지 해지 가능합니다.</p>
+            <Membership/>
         </main>
     );
 };
