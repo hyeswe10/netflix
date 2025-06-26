6f948cbaab1320e2c8fbab0958f3cf6a19fac24f
hyeswe10 <jmoon981107@gmail.com>
2025-06-26 12:36:56 +0900

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import MainLayout from "./Component/MainLayout";
 import Main from "./Component/Main";
 import Login from "./Component/Login";
 const App = () => {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Main />} />
           <Route path="login" element={<Login />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+      </HashRouter>
+    // </BrowserRouter>
   );
 };
 
